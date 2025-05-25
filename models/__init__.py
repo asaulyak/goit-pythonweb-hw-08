21919316720c3118c8b1037fe1eaf13c597f89bb
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2025-05-25 10:34:07 +0300

    add initial migration

---
+++ models/__init__.py
@@ -0,0 +1,1 @@
+from .contacts_model import Contacts
---
+++ models/contacts_model.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Integer, String, DateTime, func
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import Mapped, mapped_column
+
+from db import Base
+
+
+class Contacts(Base):
+    __tablename__ = "contacts"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(50))
+    last_name: Mapped[str] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(120), unique=True)
+    phone: Mapped[str] = mapped_column(String(12))
+    birth_day: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
+    meta: Mapped[dict] = mapped_column(JSONB),
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
+    updated_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
